2d813af248b109fd0f971890cca7b581c2443f85
Mykola-Ivanov <mykolaivanov0201@gmail.com>
2021-08-30 14:32:30 +0300

    Добавьте файлы проекта.

---
+++ SFChat/main_form.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+
+
+int main(int argc, char** argv) {
+    
+	return 0;
+}
